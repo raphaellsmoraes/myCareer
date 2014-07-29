10c12d0fb710bc5832874b96d594560435efa08e
Raphael Moraes <raphael.lsmoraes@gmail.com>
2014-07-29 16:48:40 -0300

    Adding ONET webservice

---
+++ mycareer/src/main/java/com/rm/mycareer/model/Career.java
@@ -0,0 +1,113 @@
+package com.rm.mycareer.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by vntramo on 7/29/2014.
+ */
+public class Career {
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public ArrayList<String> getTitle() {
+        return title;
+    }
+
+    public void setTitle(ArrayList<String> title) {
+        this.title = title;
+    }
+
+    public String getWhat_they_do() {
+        return what_they_do;
+    }
+
+    public void setWhat_they_do(String what_they_do) {
+        this.what_they_do = what_they_do;
+    }
+
+    public ArrayList<String> getOn_the_job() {
+        return on_the_job;
+    }
+
+    public void setOn_the_job(ArrayList<String> on_the_job) {
+        this.on_the_job = on_the_job;
+    }
+
+    public String getKnowledgeUrl() {
+        return knowledgeUrl;
+    }
+
+    public void setKnowledgeUrl(String knowledgeUrl) {
+        this.knowledgeUrl = knowledgeUrl;
+    }
+
+    public String getSkillsUrl() {
+        return skillsUrl;
+    }
+
+    public void setSkillsUrl(String skillsUrl) {
+        this.skillsUrl = skillsUrl;
+    }
+
+    public String getAbilitiesUrl() {
+        return abilitiesUrl;
+    }
+
+    public void setAbilitiesUrl(String abilitiesUrl) {
+        this.abilitiesUrl = abilitiesUrl;
+    }
+
+    public String getPersonalityUrl() {
+        return personalityUrl;
+    }
+
+    public void setPersonalityUrl(String personalityUrl) {
+        this.personalityUrl = personalityUrl;
+    }
+
+    public String getTechnologyUrl() {
+        return technologyUrl;
+    }
+
+    public void setTechnologyUrl(String technologyUrl) {
+        this.technologyUrl = technologyUrl;
+    }
+
+    public String getEducationUrl() {
+        return educationUrl;
+    }
+
+    public void setEducationUrl(String educationUrl) {
+        this.educationUrl = educationUrl;
+    }
+
+    public String getJobOutlookUrl() {
+        return jobOutlookUrl;
+    }
+
+    public void setJobOutlookUrl(String jobOutlookUrl) {
+        this.jobOutlookUrl = jobOutlookUrl;
+    }
+
+    public String code;
+    public ArrayList<String> title;
+    public String what_they_do;
+    public ArrayList<String> on_the_job;
+    public String knowledgeUrl;
+    public String skillsUrl;
+    public String abilitiesUrl;
+    public String personalityUrl;
+    public String technologyUrl;
+    public String educationUrl;
+    public String jobOutlookUrl;
+
+    public Career() {
+    }
+
+}
--- mycareer/src/main/java/com/rm/mycareer/utils/ONETXmlReader.java
+++ mycareer/src/main/java/com/rm/mycareer/utils/ONETXmlReader.java
@@ -2,49 +2,30 @@ package com.rm.mycareer.utils;
 
 import android.util.Base64;
 
+import com.rm.mycareer.model.Career;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by vntramo on 7/23/2014.
  */
 public class ONETXmlReader {
 
-    private String code = "code";
-    private String title = "title";
-    private String what_they_do = "what_they_do";
-    private String on_the_job = "on_the_job";
-    private String task = "task";
     private String urlString = null;
     private XmlPullParserFactory xmlFactoryObject;
     public volatile boolean parsingComplete = true;
 
+    public Career mCurrentCareer;
+    public ArrayList<Career> mCareerList;
+
     public ONETXmlReader(String url) {
         this.urlString = url;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getWhat_they_do() {
-        return what_they_do;
-    }
-
-    public String getOn_the_job() {
-        return on_the_job;
-    }
-
-    public String getTask() {
-        return task;
     }
 
     public void parseXMLAndStoreIt(XmlPullParser myParser) {
@@ -55,10 +36,14 @@ public class ONETXmlReader {
             while (event != XmlPullParser.END_DOCUMENT) {
                 String name = myParser.getName();
                 switch (event) {
+
                     case XmlPullParser.START_TAG:
+                        android.util.Log.d("TESTE", "Name: " + name);
                         break;
+
                     case XmlPullParser.TEXT:
                         text = myParser.getText();
+                        android.util.Log.d("TESTE", "Text: " + text);
                         break;
 
                     case XmlPullParser.END_TAG:
@@ -72,6 +57,8 @@ public class ONETXmlReader {
                             on_the_job = myParser.getAttributeValue(null, "value");
                         } else if (name.equals("task")) {
                             task = myParser.getAttributeValue(null, "value");
+                        } else if (name.equals("career")) {
+                            career = myParser.getAttributeValue(null, "value");
                         } else {
                         }
                         break;
@@ -80,14 +67,18 @@ public class ONETXmlReader {
 
             }
             parsingComplete = false;
-        } catch (Exception e) {
+        } catch (
+                Exception e
+                )
+
+        {
             e.printStackTrace();
         }
 
     }
 
-    public void fetchXML(){
-        Thread thread = new Thread(new Runnable(){
+    public void fetchXML() {
+        Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
